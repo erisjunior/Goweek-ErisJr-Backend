387fd329e9c594fcffdc1f862498648a207fa473
eris.junior <erisvan.junior.a@gmail.com>
2018-11-06 15:48:48 -0300

    Servidor Tweets e Likes Assíncrono

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect(
+    'mongodb://erisjr:zack1254@ds217360.mlab.com:17360/goweek-erisjr', 
+    {
+        useNewUrlParser: true
+    }
+);
+
+app.use((req, res, next) => {
+    req.io = io;
+    
+    return next();
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(require('./routes'));
+
+server.listen(3000);
